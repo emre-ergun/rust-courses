14e08dad9b95c7db747161ffd99348ae019fc9a1
emre <emreergun@engramsoft.com>
2023-05-06 13:45:08 +0000

    main function updated to show different variable types

--- Ultimate-Rust-Crash-Course/hello/src/main.rs
+++ Ultimate-Rust-Crash-Course/hello/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let bunnies = 2;
+    let (bunny, carrot) = (1, 5);
+
+    const TEST_CONST: i32 = 5;
+
+    println!("bunnies:{}, bunny:{}, carrot:{}", bunnies, bunny, carrot);
+    println!("Test Constant:{}", TEST_CONST);
 }
