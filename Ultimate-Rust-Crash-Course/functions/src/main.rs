2a9ce62f7f8e34c9fff553ae52c463fd489221e4
emre <emreergun@engramsoft.com>
2023-05-06 14:24:50 +0000

    exercise b added for functions

---
+++ Ultimate-Rust-Crash-Course/functions/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_variables)]
+
+fn main() 
+{
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+    let area = area_of(width, height);
+    
+    println!("Area is {}", area);
+    println!("Volume is {}", volume(width, height, depth));
+}
+
+fn area_of(x: i32, y: i32) -> i32
+{
+    return x * y;
+}
+
+fn volume(x: i32, y: i32, z: i32) -> i32
+{
+    return area_of(x, y) * z;
+}
