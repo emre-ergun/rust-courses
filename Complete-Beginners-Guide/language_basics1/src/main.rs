f07d5ae5e00f16016c168c2928d2cfbfea813725
emre <emreergun@engramsoft.com>
2023-05-11 21:46:48 +0000

    new project created to follow language basics subject

---
+++ Complete-Beginners-Guide/language_basics1/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let name = "Emre"; //type as default is &str
+    let age = 34; //type as default is i32
+
+    //explicitly
+    let value: u32 = 46; //type is specified explicitly as u32
+}
